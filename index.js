e10710db3e20bf7921610f90f18444cf9f283cb2
Brandon Eleonora <brandoneleonora38@gmail.com>
2023-04-09 09:12:37 -0400

    complete project

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+const main = document.getElementById('main')
+
+main.remove()
+
+const newHeader = document.createElement('h1')
+
+newHeader.setAttribute('id', 'victory');
+
+newHeader.innerHTML = 'is the champion'
+
+document.body.append(newHeader);
+
+
